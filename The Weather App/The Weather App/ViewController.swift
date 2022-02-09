3cf8c63a4c8537f3398b500faa2a499d5444c7a3
Utkrist Karky <63005844+utkrist-karky@users.noreply.github.com>
2022-02-09 12:04:02 +0100

    Xcode init

---
+++ The Weather App/The Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Weather App
+//
+//  Created by Macbook_Pro on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
